3667a6180e9179500017bfea09eb463fcfc8bce9
gaoridang2 <monooi@naver.com>
2023-06-04 18:35:21 +0900

    Installing and setting react router dom

---
+++ src/modules/myTheme.ts
@@ -0,0 +1,23 @@
+import { DefaultTheme } from 'styled-components';
+
+export const myTheme: DefaultTheme = {
+  color: {
+    background: '#fff',
+  },
+
+  fontSize: {
+    xl: '4rem',
+    ll: '2rem',
+    ml: '1rem',
+    rl: '0.8rem',
+    sl: '0.6rem',
+  },
+  fontWeight: {
+    extraBold: 900,
+    bold: 700,
+    semiBold: 600,
+    medium: 500,
+    reguler: 400,
+    thin: 300,
+  },
+};
---
+++ src/modules/styled.d.ts
@@ -0,0 +1,26 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    color: {
+      background: string;
+    };
+
+    fontSize: {
+      xl: string;
+      ll: string;
+      ml: string;
+      rl: string;
+      sl: string;
+    };
+
+    fontWeight: {
+      extraBold: number;
+      bold: number;
+      semiBold: number;
+      medium: number;
+      reguler: number;
+      thin: number;
+    };
+  }
+}
